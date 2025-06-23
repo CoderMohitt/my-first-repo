24d46eca7c3600bb708840a25edccdbbd1e7dec2
CoderMohitt <mohitjoshiyt@gmail.com>
2025-06-23 20:30:19 +0530

    added day4.py

---
+++ day04.py
@@ -0,0 +1,83 @@
+# #relational operators / comparison operators
+# #<, >, ==, !=, <=, >=
+# #all answers in true or false
+
+# a = 99
+# b = 100
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+# c = 100
+# d = 100
+
+# print(c != d)
+
+# is_subscribed = True
+
+# if(is_subscribed == True):
+#     print("you are a subscription holder")
+
+# print("you are not a subscription holder")
+
+# age = 17
+
+# if(age >= 18):
+#     print("you can vote")
+# else:
+#     print("you cant vote")
+
+# is_subscribed = False
+
+# if(is_subscribed == True):
+#     print("you are a subscription holder")
+# else:
+#     print("you are not a subscription holder") 
+
+# marks = 59
+
+# print(marks >= 60)
+
+# if(marks >= 60):
+#     print("you are pass")
+# else:
+#     print("you are fail")
+
+# num = 4
+
+# print(num % 2)
+
+# print(num % 2 == 0)
+
+# if(num % 2 != 0):
+#     print("number is odd")
+# else:
+#     print("number is even")
+
+# age = 11
+
+# if(age >= 18):
+#     print("you can vote")
+# elif(age <= 10):
+#     print("you are a kid")
+# else:
+#     print("you cant vote")
+
+marks = 100
+
+#30 or less, padh le bhai
+
+if(marks >= 90):
+    print('A')
+if(marks >= 80):
+    print('B')
+if(marks >= 70):
+    print('C')
+if(marks >= 60):
+    print('D')
+if(marks <= 30):
+    print('padh le bhai')
