d7dda24be70e4a42084897e5ff9063db6400788f
CoderMohitt <mohitjoshiyt@gmail.com>
2025-06-23 20:22:56 +0530

    added python file

---
+++ day2.py
@@ -0,0 +1,74 @@
+# # # print("hello world")
+
+# # # name = "john"
+# # # age = 20
+# # # age = 23
+# # # name = "xyz"
+# # # name = "abc"
+# # # # print(name)
+# # # # print(age)
+
+# # # print(name, age)
+# # # x = 10
+
+# # #string data type
+# # name = "john"
+
+# # #integer data type
+# # number = 10
+
+# # #floating point numbers
+# # abc = 10.123
+
+# # #boolean
+# # value = True
+# # value = False
+
+# # is_student = True
+# # is_student = False
+
+# # print(name)
+# # print(number)
+# # print(abc)
+# # print(value)
+
+# # print(type(name))
+# # print(type(number))
+# # print(type(abc))
+# # print(type(value))
+
+# # a = '134'
+# # a = "134"
+# # # print(type(a))
+# # print("abc " * 5)
+
+# a = 10
+# b = str(a) #converts integer to string
+# print(type(b))
+# print(type(a))
+
+# a = "10"
+# b = int(a)
+# print(type(b))
+
+# a = 20
+# b = float(a)
+# print(type(b))
+# print(b)
+
+# a = 20.012
+# b = int(a)
+# print(b)
+
+# price = "99"
+# print(int(price) + 1)
+
+# print(type(str(50)))
+
+num1 = int(input("enter 1st number : ")) #takes input as string
+num2 = int(input("Enter 2nd number : "))
+
+print('done ' * num2) #
+# print("this" + "is" + "nit")
+# num1 -> 10
+# num2 -> 10
