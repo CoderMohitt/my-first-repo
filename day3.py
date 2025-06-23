4b1faa0139c73473e5465670e362ca9db6c229c7
CoderMohitt <mohitjoshiyt@gmail.com>
2025-06-23 20:25:11 +0530

    added day3.py

---
+++ day3.py
@@ -0,0 +1,57 @@
+# # print("user input")
+
+# # print(2 + 3)
+
+# # print('done') #no user involvement
+
+# # name = input("Enter Your Name : ")
+# # num1 = input("Enter first number : ")
+# # num2 = input("Enter second number : ")
+
+# # num3 = int(num1)
+# # num4 = int(num2)
+
+# # print(type(nu/ num4) #3.5555 5.9999
+
+# #this is python, expect the unexpected
+
+# # print("mohit" + "joshi")
+
+# # userinput -> age 19 output -> next year age will be 20
+
+# # age = int(input('Enter Your Age : '))
+# # print("next year your age will be", age + 1)
+
+# user_input = int(input("Enter an integer value: "))
+# result = user_input + 5.5
+# print("The result after adding 5.5 is:", result)
+
+#basic arithmetic operators
+
+#+, -, *, /, % -> modulo operator
+
+a = 28
+b = 5
+
+#15 / 5 = 0
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(int(a / b))
+# print(a % b)
+# print(28 // 5) #floor division
+
+a = 10
+b = 20
+c = 30
+
+avg =( a + b + c )/ 3
+#10 + 20 + 10
+
+
+print(avg)
+
+print((10 + 20) / 5 + 5 * 4)
+#10 + 4 + 20
+#6 + 20
